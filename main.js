6c4c8439ba77286fd997ec91f4452ba260c3eda2
carolina quevedo <quevedocaro>
2019-06-21 08:05:46 -0500

    aprendiendo for

--- main.js
+++ main.js
@@ -1,4 +1,48 @@
-alert("estamos empezando a usar git")
+var arreglocarros=[
+	{
+		marca:"BMW",
+		referencia:"M3",
+		modelo:2015,
+		numchasis:2345	
+	},
+	{
+		marca:"AUDI",
+		referencia:"A3",
+		modelo:2019,
+		numchasis:4567
+	},
+	{
+		marca:"CHEVROLET",
+		referencia:"CAPTIVA",
+		modelo:2017,
+		numchasis:56787
+	}
+]
+
+
+for (var i = 0; i < arreglocarros.length; i++) 
+	{	
+		if (arreglocarros[i].modelo < 2019) {
+			alert("Su carro es"+ 
+				arreglocarros[i].modelo+
+				arreglocarros[i].marca)
+			alert("Tiene que pagar 100mil de tecnomecanica")
+		}else{
+			alert("Su carro es"+ 
+				arreglocarros[i].modelo+
+				arreglocarros[i].marca)
+			alert("su carro es muy nuevo debe pagar 300mil")
+
+		}
+		/*alert(arreglocarros[i].marca+" "+
+			arreglocarros[i].modelo)*/
+	}
+
+
+
+
+
+/*alert("estamos empezando a usar git")
 
 console.log("ahora vamos a subir nuestros archivos")
 
@@ -17,3 +61,4 @@ alert("el nombre es: " + persona.nombre)
 	}else{
 		alert(persona.nombre+" es menor de edad")
 	}
+*/
