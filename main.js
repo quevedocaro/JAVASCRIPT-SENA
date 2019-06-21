2d2feb5ed3aa8d1c90338170ecc77a00736ecfb8
carolina quevedo <quevedocaro>
2019-06-21 06:41:54 -0500

    aprendiendo estructuras

---
+++ main.js
@@ -0,0 +1,19 @@
+alert("estamos empezando a usar git")
+
+console.log("ahora vamos a subir nuestros archivos")
+
+var persona ={	
+	nombre:"chavo", 
+	apellido:"del ocho",
+	cedula: "123456789",
+	edad:15
+	}
+
+alert("el nombre es: " + persona.nombre)
+
+	if(persona.edad >=18)
+	{
+		alert(persona.nombre+" es mayor de edad")
+	}else{
+		alert(persona.nombre+" es menor de edad")
+	}
